5070203d0fd49219ef9e20ff62740107c318c762
Clarence-Hugues Domond <clarencehd@outlook.com>
2021-02-19 22:41:52 -0800

    Implemented extra features, such as youtube video playback and improved UI

--- app/src/main/java/com/codepath/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/flixster/adapters/MovieAdapter.java
@@ -1,27 +1,35 @@
 package com.codepath.flixster.adapters;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.util.Log;
+import androidx.core.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.target.Target;
+import com.codepath.flixster.DetailActivity;
 import com.codepath.flixster.R;
 import com.codepath.flixster.models.Movie;
 
+import org.parceler.Parcels;
+import org.w3c.dom.Text;
+
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
@@ -60,6 +68,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -69,22 +78,50 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
+            int radius = 30; // corner radius, higher value = more rounded
             // If phone is in landscape
             // then imageUrl = back drop image
             // else imageUrl = poster image
             if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                // Register click listener on whole container row
+                // Navigate to a new activity
                 imageUrl = movie.getBackdropPath();
+                int margin = 10; // crop margin, set to 0 for corners with no crop
+                Glide.with(context)
+                        .load(imageUrl)
+                        .transform(new RoundedCornersTransformation(radius, margin))
+                        .override(1000, Target.SIZE_ORIGINAL)
+                        .into(ivPoster);
             } else {
+                // Register click listener on whole container row
+                // Navigate to a new activity
                 imageUrl = movie.getPosterPath();
+                Glide.with(context)
+                        .load(imageUrl)
+                        .transform(new RoundedCorners(radius))
+                        .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+                        .into(ivPoster);
             }
 
-            Glide.with(context).load(imageUrl).into(ivPoster);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    Pair<View, String> p1 = Pair.create(tvTitle, "information");
+                    Pair<View, String> p2 = Pair.create(tvOverview, "description");
+                    ActivityOptionsCompat options = ActivityOptionsCompat.
+                            makeSceneTransitionAnimation((Activity)context, p1, p2);
+                    context.startActivity(i, options.toBundle());
+                }
+            });
         }
     }
 }
